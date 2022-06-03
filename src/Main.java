09700154f51086931f18d0408f8e641513474049
Maximiliano Mendez <maximiliano.mendez@vemobility.net>
2022-06-03 18:19:51 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int res = sum(1, 2, 3);
+        System.out.println(res);
+
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
